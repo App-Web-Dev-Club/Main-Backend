107d7ac4379d135f3080ff717d48ef50dfa41201
Chijith Jerin Stalin <chijithjerin2004@gmail.com>
2024-02-12 17:33:13 +0530

    ....

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,15 +6,7 @@ urlpatterns = [
 
     # migrated from prototype -------------------------
     
-    path('projects', ProjectListCreateAPIView.as_view(), name='project-list-create'),
-    path('userid',Userid.as_view(),name='get userid using regno used for both project and attendance'),
-    path('userid/projects',project_under_user.as_view(),name='get project from userid used for attendance'),
-    path('attendance', AttendanceListCreateAPIView.as_view(), name='attendance-list-create'),
-    path('process',PermissionView.as_view()),
-    # path('face',FaceAttendanceListCreateAPIView.as_view()),
-    path('punch', PunchTimeView.as_view()),
-    # path('punchtime/sort',PunchTimeGETView.as_view()),
-    # path('send-email/', SendEmailView.as_view(), name='send-email'),
+    
 
     #--------------------------------
 
--- backend/kids/urls.py
+++ backend/kids/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
+# from .views import *
+from kids.views import *
 
 urlpatterns = [
 
-
+    path('projects', ProjectListCreateAPIView.as_view(), name='project-list-create'),
+    path('userid',Userid.as_view(),name='get userid using regno used for both project and attendance'),
+    path('userid/projects',project_under_user.as_view(),name='get project from userid used for attendance'),
+    path('attendance', AttendanceListCreateAPIView.as_view(), name='attendance-list-create'),
+    path('process',PermissionView.as_view()),
+    # path('face',FaceAttendanceListCreateAPIView.as_view()),
+    path('punch', PunchTimeView.as_view()),
+    # path('punchtime/sort',PunchTimeGETView.as_view()),
+    # path('send-email/', SendEmailView.as_view(), name='send-email'),
 ]
+
