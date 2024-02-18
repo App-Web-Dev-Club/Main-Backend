6eb5a0671644eebc016ef614fc632a455ba35a09
Chijith Jerin Stalin <chijithjerin2004@gmail.com>
2024-02-18 12:35:00 +0530

    Creating API endpoints

--- backend/inventory/urls.py
+++ backend/inventory/urls.py
@@ -1,7 +1,7 @@
-
 from django.urls import path
+from inventory.views import *
 
 urlpatterns = [
-
+    path('product', ProductAPIView.as_view(), name='Register Product Details'),
 
 ]
--- backend/inventory/views.py
+++ backend/inventory/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from inventory.models import *
+from kids.models import *
+from inventory.serializers import *
 # Create your views here.
+
+class ProductAPIView(APIView):
+
+    def get(self, request):
+        data = Product.objects.all()
+        serializer = ProductSerializer(data, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        serializer = ProductSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
