343735be3b8a6d31467a2ef19d9f60d34ed57e5c
Chijith Jerin Stalin <chijithjerin2004@gmail.com>
2024-03-19 18:08:15 +0530

    The "PermissionView" class retrieves data from the "PunchView" class and generates a PDF, which is then emailed.

--- backend/kids/pdfgenerator.py
+++ backend/kids/pdfgenerator.py
@@ -16,25 +16,30 @@ def generate_permission_pdf(users):
 
     today_date = datetime.date.today().strftime("%d-%m-%Y")
     logo_path = "/home/jerin/Documents/Development/Main-Backend/backend/kids/images/KH_Logo.png"
+    stamp_path = "/home/jerin/Documents/Development/Main-Backend/backend/kids/images/stamp.png"
+
+
 
     # Write the content to the PDF
-    p.drawImage(logo_path, x=50, y=700,width=250,height=70,mask='auto')
-    p.drawString(60, 650, "From") 
-    p.drawString(450, 650, f"Date: {today_date}")  
-    p.drawString(75, 620, "Head CTC/KIDS,")  
-    p.drawString(75, 595, "Karunya Institute of Technology and Sciences.")  
+    p.drawImage(logo_path, x=370, y=710,width=180,height=50,mask='auto')
+    p.drawImage(stamp_path, x=50, y=700,width=220,height=60,mask='auto')
+
+    p.drawString(60, 650-60, "From") 
+    p.drawString(450, 650-60, f"Date: {today_date}")  
+    p.drawString(75, 620-60, "Head CTC/KIDS,")  
+    p.drawString(75, 595-60, "Karunya Institute of Technology and Sciences.")  
     
-    p.drawString(60, 550, "To")  
-    p.drawString(75, 520, "The Chief Warden (Boys Hostel),")  
-    p.drawString(75, 495, "Karunya Institute of Technology and Science,")  
-    p.drawString(75, 470, "Coimbatore-641114")  
+    p.drawString(60, 550-60, "To")  
+    p.drawString(75, 520-60, "The Chief Warden (Boys Hostel),")  
+    p.drawString(75, 495-60, "Karunya Institute of Technology and Science,")  
+    p.drawString(75, 470-60, "Coimbatore-641114")  
     
-    p.drawString(60, 425, "Subject")  
-    p.drawString(75, 395, "Re: Permission for late residence entry") 
+    p.drawString(60, 425-60, "Subject")  
+    p.drawString(75, 395-60, "Re: Permission for late residence entry") 
     
-    p.drawString(60, 350, "Respected Sir,")
-    p.drawString(75,320,f"The following students were working late at CTC on {today_date} Till 7:30 PM.")
-    p.drawString(75,300,"Kindly permit them to enter the hostel.")
+    p.drawString(60, 350-60, "Respected Sir,")
+    p.drawString(75,320-60,f"The following students were working late at CTC on {today_date} Till 7:30 PM.")
+    p.drawString(75,300-60,"Kindly permit them to enter the hostel.")
 
     # The following students were working late at CTC on 12-03-2024 Till 7:30 PM . Kindly permit  them to enter the hostel. 
     data = [['SI No.', 'Name', 'Register No.']]
@@ -43,8 +48,19 @@ def generate_permission_pdf(users):
         register_no = user['regno']['register_no']
         data.append([index, name, register_no])
     
-    # Create the table
-    table = Table(data)
+    ## Set default column width
+    default_col_width = 130
+    
+    # Calculate column widths dynamically based on the maximum content width in each column
+    col_widths = [default_col_width] * len(data[0])
+    for row in data:
+        for i, cell in enumerate(row):
+            cell_width = p.stringWidth(str(cell), "Helvetica", 12) + 20
+            if cell_width > col_widths[i]:
+                col_widths[i] = cell_width
+    
+    # Create the table with dynamic column widths
+    table = Table(data, colWidths=col_widths)
     table.setStyle(TableStyle([('BACKGROUND', (0, 0), (-1, 0), colors.grey),
                                ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
                                ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
@@ -54,11 +70,11 @@ def generate_permission_pdf(users):
                                ('GRID', (0, 0), (-1, -1), 1, colors.black)]))
     
     # Draw the table on the canvas
-    table.wrapOn(p, 500, 500)
-    table.drawOn(p, 50, 200)
+    table.wrapOn(p, width, height)
+    table.drawOn(p, (width - sum(col_widths)) / 2, 200-60)
 
     table_height = sum(table._rowHeights) + len(table._rowHeights) * 1  # Total height of the table
-    y_position = 200 - table_height - 10  # 30 units below the table
+    y_position = 200 - table_height - 10 - 40  # 40 units below the table
 
     # Calculate the x-coordinate for centering the text
     text_width = p.stringWidth("Thank You.", "Helvetica", 12)
